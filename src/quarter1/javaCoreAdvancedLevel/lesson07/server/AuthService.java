31c49eb5aa6531da866eb7700b379c7bed7bf5c2
Vladimir Svishch <5693031@gmail.com>
2020-07-04 23:47:03 +0300

    hw-02-07 (Урок 7. Написание сетевого чата. Часть I) Java Core. Продвинутый уровень

---
+++ src/quarter1/javaCoreAdvancedLevel/lesson07/server/AuthService.java
@@ -0,0 +1,5 @@
+package quarter1.javaCoreAdvancedLevel.lesson07.server;
+
+public interface AuthService {
+    String getNicknameByLoginAndPassword(String login, String password);
+}
---
+++ src/quarter1/javaCoreAdvancedLevel/lesson07/server/Start.java
@@ -0,0 +1,7 @@
+package quarter1.javaCoreAdvancedLevel.lesson07.server;
+
+public class Start {
+    public static void main(String[] args) {
+        new Server();
+    }
+}
