076aefc0cd490e89e29aea749257546c583e5632
Vladimir Svishch <5693031@gmail.com>
2020-07-08 15:46:57 +0300

    hw-2-08 Урок 8. Написание сетевого чата. Часть II Java Core. Продвинутый уровень

---
+++ src/quarter1/javaCoreAdvancedLevel/lesson08/MainLesson08.java
@@ -0,0 +1,30 @@
+package quarter1.javaCoreAdvancedLevel.lesson08;
+
+import quarter1.javaCoreAdvancedLevel.lesson07.server.Server;
+
+public class MainLesson08 {
+
+   /*
+        ФИО:  		    Свищ Владимир Сергеевич
+        Факультет:      Geek University Android-разработки
+        Курс: 		    Java Core. Продвинутый уровень
+        Урок: 		    Урок 8. Написание сетевого чата. Часть II
+        Дата правки:    08.07.2020
+    */
+
+    public static void main(String[] args) {
+
+    /*
+           Задание.
+
+        1. Разобраться с кодом.
+        2. Добавить отключение неавторизованных пользователей по таймауту
+        (120 сек. ждем после подключения клиента. Если он не авторизовался
+        за это время, закрываем соединение).
+
+    */
+
+        new Server();
+
+    }
+}
